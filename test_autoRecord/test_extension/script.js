7d783307f16b9e0c0ef27e3ebe815d22cceaa46f
shine <263397787@qq.com>
2019-07-05 15:24:29 +0800

    加入插件向网页插入元素的功能

--- test_autoRecord/test_extension/script.js
+++ test_autoRecord/test_extension/script.js
@@ -1,21 +1,34 @@
-﻿try{
-    url = 'http://127.0.0.1:5000/info'
-    $.get(url,function(data,status){
-        console.log("Data: " + data + "  Status: " + status);
-        data = data.replace(/'/g,'"');
-        return_data = $.parseJSON(data);
-        console.log(return_data.name);
-    });
-    url = 'http://127.0.0.1:5000/setValue'
-    $.ajax({
-        type: 'POST',
-        url: url,
-        data: 'this is chrome extension post data!',
-        success: postCallback
-    });
-    function postCallback(data,status){
-        console.log("this is post back Data: " + data + "Status: " + status);
+﻿function getinfo(){
+    try{
+        url = 'http://127.0.0.1:5000/info'
+        $.get(url,function(data,status){
+            console.log("Data: " + data + "  Status: " + status);
+            data = data.replace(/'/g,'"');
+            return_data = $.parseJSON(data);
+            console.log(return_data.name);
+        });
+        url = 'http://127.0.0.1:5000/setValue'
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: 'this is chrome extension post data!',
+            success: postCallback
+        });
+        function postCallback(data,status){
+            console.log("this is post back Data: " + data + "Status: " + status);
+        }
+    }catch(e){
+        console.log(e)
     }
-}catch(e){
-    console.log(e)
 }
+function insertNode(){
+    s_btn_wr = $(".s_btn_wr")[0];
+    input = s_btn_wr.firstElementChild.cloneNode();
+    input.value ='看一下';
+    s_btn_wr.appendChild(input);
+}
+
+$( document ).ready(function() {
+    insertNode();
+    getinfo();
+});
