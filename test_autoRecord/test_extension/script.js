2886d4c1b79b2481beecd694bea51522e90e4bb4
shine <263397787@qq.com>
2019-07-05 08:55:30 +0800

    加入插件送服务器推送值的功能。

--- test_autoRecord/test_extension/script.js
+++ test_autoRecord/test_extension/script.js
@@ -1,11 +1,21 @@
-﻿console.log('start test!')
-try{
-    $.get("http://127.0.0.1:5000/info",function(data,status){
-        alert("Data: " + data + "nStatus: " + status);
+﻿try{
+    url = 'http://127.0.0.1:5000/info'
+    $.get(url,function(data,status){
+        console.log("Data: " + data + "  Status: " + status);
         data = data.replace(/'/g,'"');
         return_data = $.parseJSON(data);
         console.log(return_data.name);
     });
+    url = 'http://127.0.0.1:5000/setValue'
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: 'this is chrome extension post data!',
+        success: postCallback
+    });
+    function postCallback(data,status){
+        console.log("this is post back Data: " + data + "Status: " + status);
+    }
 }catch(e){
     console.log(e)
 }
--- test_autoRecord/test_flask.py
+++ test_autoRecord/test_flask.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.json import jsonify
 from uuid import uuid4
 from flask_cors import CORS
+from flask import request
 
 app= Flask(__name__)
 CORS(app)
@@ -10,6 +11,13 @@ CORS(app)
 def info():
     response = "{'test':123, 'name':'test'}"
     return response,200
+
+@app.route('/setValue', methods= ['POST'])
+def setValue():
+    value = request.get_json()
+    print(value)
+    return "set successful!",201
+
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port = 5000)
